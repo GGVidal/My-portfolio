5d87d77c21571c3e9e83097c76561f25d3463865
GGVidal <gabrielvidalgondim@gmail.com>
2020-08-03 19:30:10 -0300

    Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,69 @@
-import React from "react";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  FacebookOutlined,
+  UserOutlined,
+  LinkedinOutlined,
+  GithubOutlined,
+} from '@ant-design/icons';
+import React, { useState } from 'react';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const App = () => {
-  return <div>Hi world</div>;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    console.log(collapsed);
+    setCollapsed(collapsed);
+  };
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <Menu.Item key="9" icon={<FileOutlined />} />
+          <SubMenu key="sub1" icon={<UserOutlined />} title="Social Medias">
+            <Menu.Item key="2" icon={<LinkedinOutlined />}>
+              <a href="https://www.linkedin.com/in/gabriel-gondim-584477157/">
+                LinkedIn
+              </a>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<GithubOutlined />}>
+              <a href="https://github.com/GGVidal">Github</a>
+            </Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 };
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
