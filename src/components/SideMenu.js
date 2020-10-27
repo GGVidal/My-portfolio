a5197140f3a1772aa6a6e7202b902a987e4366f1
GGVidal <gabrielvidalgondim@gmail.com>
2020-10-26 22:42:22 -0300

    CHG: Menu component

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
-import {
-  DesktopOutlined,
-  OrderedListOutlined,
-  FileOutlined,
-  UserOutlined,
-  LinkedinOutlined,
-  GithubOutlined,
-  MailOutlined,
-  PhoneOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 
 import Thumbnail from './Thumbnail';
+import Menu from './Menu';
 const { Sider } = Layout;
-const { SubMenu } = Menu;
 
 const SideMenu = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -29,26 +19,7 @@ const SideMenu = () => {
       width={250}
     >
       <Thumbnail />
-      <div className="logo" />
-      <Menu theme="dark" mode="inline">
-        <Menu.Item key="1" icon={<OrderedListOutlined />}>
-          Timeline
-        </Menu.Item>
-        <Menu.Item key="2" icon={<DesktopOutlined />}>
-          About
-        </Menu.Item>
-        <Menu.Item key="9" icon={<FileOutlined />} />
-        <SubMenu key="sub1" icon={<UserOutlined />} title="Social Medias">
-          <Menu.Item key="2" icon={<LinkedinOutlined />}>
-            <a href="https://www.linkedin.com/in/gabriel-gondim-584477157/">
-              LinkedIn
-            </a>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<GithubOutlined />}>
-            <a href="https://github.com/GGVidal">Github</a>
-          </Menu.Item>
-        </SubMenu>
-      </Menu>
+      <Menu />
     </Sider>
   );
 };
